153e2e49f5f341f55b82228c50a14f8aa344a662
Cuneyt Turker <cuneytturker35@gmail.com>
2022-11-03 22:46:15 +0000

    dropdown menu
    Dropdown menu has been created

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let container = document.getElementById("cntn")
 let searchBar = document.getElementById("searchBar");
 let allEpisodes = getAllEpisodes();
+let displayCount = document.createElement("h5")
+    displayCount.setAttribute("id", "displaying");
+    displayCount.setAttribute("class", "ml-3")
+let box = document.getElementById("boxe");
+box.appendChild(displayCount)
+let result = document.getElementById("displaying")
+let dropdown = document.getElementById("dropdown")
+ 
+    
+    
+let filteredObj;
+
 setup(allEpisodes)
 searchBar.addEventListener("keyup", (e) => {
   container.innerHTML = ""
   let searchValue = e.target.value.toLowerCase();
-  
   let allEps = getAllEpisodes();
   let filteredObj = allEps.filter(val => {
     return val.name.toLowerCase().includes(searchValue)
   })
-  
+  result.innerText = `Displaying ${filteredObj.length}/${allEpisodes.length}`
   setup(filteredObj)
 })
+  
 
 function setup(eps) {
 for(let i in eps){
@@ -25,18 +37,23 @@ for(let i in eps){
      <div class="card-title text-center border">
      <h5 class="card-header">${eps[i].name} - S${season}E${episode}</h5>
      </div>
-     <div class="card-text border">
+     <div class="card-text p-2">
        ${eps[i].summary}
      </div>
    </div>
    `
+
+   let option = `<a class="dropdown-item itemFound" name="${i}" href="#" onclick="itemFounder(event)">${eps[i].name}</a>`
+   dropdown.insertAdjacentHTML("beforeend",option)
    container.insertAdjacentHTML("beforeend",card)
-
-}
-}
-
-function makePageForEpisodes(episodeList) {
   
+   
+}
 }
 
 
+function itemFounder(e){
+  let getId = e.target.name;
+  console.log(getId)
+  e.target.setAttribute("href", `#${getId}`)
+ }
