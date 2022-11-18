7d1a48a45af7cd58091e937619c31e71447d5b27
Cuneyt Turker <cuneytturker35@gmail.com>
2022-11-18 03:39:37 +0000

    tv show

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let container = document.getElementById("cntn")
 let searchBar = document.getElementById("searchBar");
-let allEpisodes = getAllEpisodes();
+let allShows = getAllShows();
 let displayCount = document.createElement("h5")
     displayCount.setAttribute("id", "displaying");
     displayCount.setAttribute("class", "ml-3")
@@ -8,32 +8,37 @@ let box = document.getElementById("boxe");
 box.appendChild(displayCount)
 let result = document.getElementById("displaying")
 let dropdown = document.getElementById("dropdown")
+let dropdown2 = document.getElementById("dropdown2")
     
-let filteredObj;
-setup(allEpisodes)
-searchBar.addEventListener("keyup", (e) => {
-  container.innerHTML = ""
-  let searchValue = e.target.value.toLowerCase();
-  let allEps = getAllEpisodes();
-  let filteredObj = allEps.filter(val => {
-    return val.name.toLowerCase().includes(searchValue)
-  })
-  result.innerText = filteredObj.length == 0 ? "Sorry, there is no result :("
-  :`Displaying ${filteredObj.length}/${allEpisodes.length}`
-  setup(filteredObj)
-})
+let card;
+setup(allShows)
+// searchBar.addEventListener("keyup", (e) => {
+//   container.innerHTML = ""
+//   let searchValue = e.target.value.toLowerCase();
+//   let allEps = getAllEpisodes();
+//   let filteredObj = allEps.filter(val => {
+//     return val.name.toLowerCase().includes(searchValue)
+//   })
+//   result.innerText = filteredObj.length == 0 ? "Sorry, there is no result :("
+//   :`Displaying ${filteredObj.length}/${allEpisodes.length}`
+//   setup(filteredObj)
+// })
   
-
+// Show dropdown elements
 function setup(eps) {
 for(let i in eps){
-  let season = eps[i].season>9?`${eps[i].season}`:`0${eps[i].season}`
-  let episode = eps[i].number>9?`${eps[i].number}`:`0${eps[i].number}`
+  // let season = eps[i].season>9?`${eps[i].season}`:`0${eps[i].season}`
+  // let episode = eps[i].number>9?`${eps[i].number}`:`0${eps[i].number}`
+  let img = eps[i].image
+  if(img == null){
+    img = ""
+  }
   let card = `
   
-  <div class="card rounded-3" style="width: 18rem;" id="${i}">
-     <img class="card-img-top rounded" src="${eps[i].image.medium}" alt="Card image cap">
+  <div class="card rounded-3 col-md-6"  id="${i}">
+     <img class="card-img-top rounded heights" src="${img.medium}" alt="Card image cap">
      <div class="card-title text-center border">
-     <h5 class="card-header bg-danger text-light">${eps[i].name} - S${season}E${episode}</h5>
+     <h5 class="card-header bg-danger text-light">${eps[i].name}</h5>
      </div>
      
      <div class="card-text p-3">
@@ -42,7 +47,7 @@ for(let i in eps){
    </div>
    `
 
-   let option = `<a class="dropdown-item itemFound" name="${i}" href="#" onclick="itemFounder(event)">${eps[i].name}</a>`
+   let option = `<a class="dropdown-item itemFound" id="${eps[i].id}">${eps[i].name}</a>`
    dropdown.insertAdjacentHTML("beforeend",option)
    container.insertAdjacentHTML("beforeend",card)
 }
@@ -60,27 +65,94 @@ for(let i in eps){
      }
    }
 }
+// Show episodes
 
-function itemFounder(e){
-  container.innerHTML =""
-  setup(allEpisodes)
-  let getId = e.target.name;
-  e.target.setAttribute("href", `#${getId}`)
-  let input = e.target.innerText;
-   
-   let summ = document.getElementsByClassName("card-text")
-   let titl = document.getElementsByClassName("card-header")
-   let exp = new RegExp(input , "gi")
- 
-   for(let i in summ){
-    
-    if(input !== ""){
+function makePageForEpisodes(eps) {
+  container.innerHTML = ""
+    for(let i in eps){
+      // let season = eps[i].season>9?`${eps[i].season}`:`0${eps[i].season}`
+      // let episode = eps[i].number>9?`${eps[i].number}`:`0${eps[i].number}`
+      let img = eps[i].image
+      if(img == null){
+        img = ""
+      }
+      let card = `
       
-      summ[i].innerHTML = summ[i].innerHTML.replace(exp,match => `<mark>${match}</mark>`)
-      titl[i].innerHTML = titl[i].innerHTML.replace(exp,match => `<mark>${match}</mark>`)
-     }
-   }
- }
+      <div class="card rounded-3 col-md-6"  id="${i}">
+         <img class="card-img-top rounded heights" src="${img.medium}" alt="Card image cap">
+         <div class="card-title text-center border">
+         <h5 class="card-header bg-danger text-light">${eps[i].name}</h5>
+         </div>
+         
+         <div class="card-text p-3">
+          ${eps[i].summary}
+         </div>
+       </div>
+       `
+    
+       let option = `<a class="dropdown-item itemFound" name="${i}">${eps[i].name}</a>`
+       dropdown2.insertAdjacentHTML("beforeend",option)
+       
+       container.insertAdjacentHTML("beforeend",card)
+    }
+       let input = searchBar.value;
+       console.log(input)
+       let summ = document.getElementsByClassName("card-text")
+       let titl = document.getElementsByClassName("card-header")
+       let exp = new RegExp(input , "gi")
+       
+       for(let i in summ){
+        if(input !== "" && input.length>2){
+          
+          summ[i].innerHTML = summ[i].innerHTML.replaceAll(exp,match => `<mark>${match}</mark>`)
+          titl[i].innerHTML = titl[i].innerHTML.replaceAll(exp,match => `<mark>${match}</mark>`)
+         }
+       }
+    
+}
+
+//EpisodeDropdown
+function getfetch(showid){
+  let url=`https://api.tvmaze.com/shows/${showid}/episodes`
+fetch(url)
+  // Get the response and extract the JSON
+  .then(function (response) {
+    return response.json();
+  })
+  // Do something with the JSON
+  .then((response) => {
+    makePageForEpisodes(response)
+
+    })}
+
+    dropdown.addEventListener('click',(e)=>{
+      console.log("asdasdasd")
+      
+        let showid=e.target.id;
+        console.log(showid)
+        getfetch(showid)
+      
+     })
+// function itemFounder(e){
+//   container.innerHTML =""
+//   setup(allEpisodes)
+//   let getId = e.target.name;
+//   e.target.setAttribute("href", `#${getId}`)
+//   let input = e.target.innerText;
+   
+//    let summ = document.getElementsByClassName("card-text")
+//    let titl = document.getElementsByClassName("card-header")
+//    let exp = new RegExp(input , "gi")
+ 
+//    for(let i in summ){
+    
+//     if(input !== ""){
+      
+//       summ[i].innerHTML = summ[i].innerHTML.replace(exp,match => `<mark>${match}</mark>`)
+//       titl[i].innerHTML = titl[i].innerHTML.replace(exp,match => `<mark>${match}</mark>`)
+//      }
+//    }
+//  }
 
  function topFunction() {
   document.body.scrollTop = 0;
