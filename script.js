961221936e2c53e1774f88a15420f6349ec99dd4
Cuneyt Turker <cuneytturker35@gmail.com>
2022-11-19 03:44:49 +0000

    Last touch

--- script.js
+++ script.js
@@ -9,9 +9,18 @@ box.appendChild(displayCount)
 let result = document.getElementById("displaying")
 let dropdown = document.getElementById("dropdown")
 let dropdown2 = document.getElementById("dropdown2")
+let home = document.getElementById("homebtn")
 
+
+// Display all of the shows and tv icon
 setup(allShows);
+home.addEventListener("click", (e)=> {
+  container.innerHTML = "";
+  setup(allShows)
+})
+home.style.cursor="pointer"
 
+// Search bar for show page
 
     searchBar.addEventListener('keyup',(event)=>{
     container.innerHTML = ""
@@ -28,22 +37,24 @@ setup(allShows);
   })
   
 // Show dropdown elements
+
 function setup(eps) {
+  
 for(let i in eps){
-  let img = eps[i].image
-  if(img == null){
-    img = ""
+  if(eps[i].image == null){
+    eps[i].image = {};
+    eps[i].image.medium = ""
   }
   let card = `
   
-  <div class="card rounded-3 col-md-12 p-0"  id="${i}">
+  <div class="card rounded-3 col-md-12 p-0" name="${eps[i].id}">
      <div style="display:flex">
-     <img class=" rounded heights" src="${img.medium}" alt="Card image cap">
-     <div class="direction ml-5">
-     <h3>Language : ${eps[i].language}</h3>
-     <h3>Rating : ${eps[i].rating.average}</h3>
-     <h3>Genres : ${eps[i].genres}</h3>
-     <h3>Status : ${eps[i].status}</h3>
+     <img class=" rounded heights" src="${eps[i].image.medium}" alt="Card image cap">
+     <div class="direction">
+     <h3 class="sizing">Language : ${eps[i].language}</h3>
+     <h3 class="sizing">Rating : ${eps[i].rating.average}</h3>
+     <h3 class="sizing">Genres : ${eps[i].genres}</h3>
+     <h3 class="sizing">Status : ${eps[i].status}</h3>
      </div>
      </div>
      
@@ -60,11 +71,9 @@ for(let i in eps){
    let option = `<a class="dropdown-item itemFound" id="${eps[i].id}">${eps[i].name}</a>`
    dropdown.insertAdjacentHTML("beforeend",option)
    container.insertAdjacentHTML("beforeend",card)
-}
-   
-   
-}
-// Show episodes
+}}
+
+// Episodes maker
 
 function makePageForEpisodes(eps) {
   container.innerHTML = ""
@@ -78,10 +87,10 @@ function makePageForEpisodes(eps) {
       }
       let card = `
       
-      <div class="card rounded-3 col-md-6 p-0"  id="${i}">
+      <div class="card rounded-3 col-md-4 p-0"  id="${i}">
          <img class="card-img-top rounded widths" src="${img.medium}" alt="Card image cap">
          <div class="card-title text-center border">
-         <h5 class="card-header markup bg-danger text-light">${eps[i].name} - S${season}-E${episode}</h5>
+         <h5 class="card-header markup bg-danger text-light">S${season}-E${episode} - ${eps[i].name}</h5>
          </div>
          
          <div class="card-text markup p-3">
@@ -90,12 +99,10 @@ function makePageForEpisodes(eps) {
        </div>
        `
     
-       let option = `<a class="dropdown-item itemFound" name="${i}">S${season}-E${episode} - ${eps[i].name} - ${eps[i].name}</a>`
+       let option = `<a class="dropdown-item itemFound" name="${i}">S${season}-E${episode} - ${eps[i].name}</a>`
        dropdown2.insertAdjacentHTML("beforeend",option)
        container.insertAdjacentHTML("beforeend",card)
-
     }
-    
 }
 
 //EpisodeDropdown
@@ -109,7 +116,10 @@ fetch(url)
   // Do something with the JSON
   .then((response) => {
     makePageForEpisodes(response)
+
     let input=document.getElementById('searchBar')
+    let spanEl=document.getElementById('count-epispde')
+    spanEl.innerText = `${response.length} results displaying..`
     input.addEventListener('keyup',(event)=>{
     let episodes=response
     const searchValue=event.target.value
@@ -118,37 +128,42 @@ fetch(url)
     || episode.summary.toLowerCase().includes(searchValue.toLowerCase()))
 
    })
-   let spanEl=document.getElementById('count-epispde')
+   
+   if(searchValue !== ""){
     spanEl.innerText=`Displaying ${filteredepisodes.length} / ${episodes.length}`
+   }else {
+    spanEl.innerText = ""
+   }
     makePageForEpisodes(filteredepisodes)
-
   })
-
-    })}
-
+ })}
+// show dropdown event listener
     dropdown.addEventListener('click',(e)=>{
       
         let showid=e.target.id;
+        searchBar.value = ""
         getfetch(showid)
       
      })
 
+//episodes dropdown event listener
      dropdown2.addEventListener("click", (e)=>{
+      searchBar.value = ""
       let getId = e.target.name;
       e.target.setAttribute("href", `#${getId}`)
       let summ = document.getElementsByClassName("markup")
       
-      let input = e.target.innerText;
-      let exp = new RegExp(input , "gi")
+      let inputs = e.target.innerText;
+      let exp = new RegExp(inputs , "gi")
       
      for(let i in summ){
-      if(input !== ""){
-      summ[i].innerHTML = summ[i].innerHTML.replace(exp,match => `<mark>${match}</mark>`)
+      if(inputs !== ""){
+      summ[i].innerHTML = summ[i].innerHTML.replaceAll(exp,match => `<mark>${match}</mark>`)
       
      }
    }
      })
-
+// back to top
  function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
